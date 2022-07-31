739c981b55e61b933c9aad9a0a96403ff1cb4443
Shalin Mehta <shalinm.app@gmail.com>
2022-07-31 11:17:51 +0100

    Can now deploy and verify contracts on ETH testnets using Hardhat

--- web3_js/hardhat/scripts/deploy.js
+++ web3_js/hardhat/scripts/deploy.js
@@ -1,29 +1,51 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { Contract } = require("ethers")
+const { ethers, run, network } = require("hardhat")
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    )
+    console.log("Deploying contract...")
+    const simpleStorage = await SimpleStorageFactory.deploy()
+    await simpleStorage.deployed()
+    console.log(`Deployed contract to: ${simpleStorage.address}`)
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+    if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+        console.log("Waiting for deployment txes...")
+        // Waiting for longer to ensure Etherscan knows about the tx
+        await simpleStorage.deployTransaction.wait(6)
+        await verify(simpleStorage.address, [])
+    }
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    const currentValue = await simpleStorage.favouriteNumber()
+    console.log(`Current value is: ${currentValue}`)
 
-  await lock.deployed();
+    const updateTransaction = await simpleStorage.store(17)
+    await updateTransaction.wait(1)
 
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+    const updatedValue = await simpleStorage.favouriteNumber()
+    console.log(`Updated value is: ${updatedValue}`)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified!")
+        } else {
+            console.log(e)
+        }
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
